6e24b5cec4201edea3ebc9742fa8d09d48c78d53
Imsandysh <033561485Lahan>
2022-02-05 16:00:32 -0500

    Filtering out necessary values from EMG sensors

---
+++ arduino_lib/EMGSensor1.js
@@ -0,0 +1,10 @@
+const { Board, Sensor } = require("johnny-five")
+const board = new Board()
+
+board.on("ready", () => {
+  const sensor = new Sensor("A0")
+
+  sensor.within([100 150], function(){
+      console.log("Amplify Finger Movement")
+  })
+})
