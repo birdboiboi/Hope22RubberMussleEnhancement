1d6e5cea33709b436b42e687d14991a9c729f53e
Imsandysh <35186843+Imsandysh@users.noreply.github.com>
2022-02-05 17:02:04 -0500

    Update EMGSensor1.js

--- arduino_lib/EMGSensor1.js
+++ arduino_lib/EMGSensor1.js
@@ -3,8 +3,8 @@ const board = new Board()
 
 board.on("ready", () => {
   const sensor = new Sensor("A0")
+  var floatVoltage
   floatVoltage = sensor*(5.0/1023.0)
   //sensor.within([100, 150], function(){
       console.log("Amplify Finger Movement")
   })
-})
