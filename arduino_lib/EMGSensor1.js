8eb1cf6588eb8f9d33facebdfe3e9cbeacb35afe
Imsandysh <35186843+Imsandysh@users.noreply.github.com>
2022-02-05 17:30:12 -0500

    Update EMGSensor1.js

--- arduino_lib/EMGSensor1.js
+++ arduino_lib/EMGSensor1.js
@@ -4,6 +4,7 @@ const board = new Board()
 board.on("ready", () => {
   const sensor = new Sensor("A0")
 
-  //sensor.within([100, 150], function(){
-      console.log(sensor)
+  sensor*(5/1023, function() {
+      console.log("Amplify Finger Movement")
   })
+})
