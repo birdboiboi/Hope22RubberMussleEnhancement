f5243e82185c5c0551bea110c756ee7c954732ea
jordan <jai36@drexel.edu>
2022-02-05 17:16:13 -0500

    added relay to emg support

--- arduino_lib/EMGSensor1_solnoid.js
+++ arduino_lib/EMGSensor1_solnoid.js
@@ -1,30 +1,31 @@
-const { Board, Sensor, Relay} = require("johnny-five")
-const board = new Board()
+var five = require("johnny-five");
+var board = new five.Board();
 
 var contract = false;
 
-board.on("ready", () => {
-  const sensor = new Sensor("A0")
-  var liftRelay = new Relay(13);
-  var liftRelay = new Relay(12);
+board.on("ready", function(){
+  const sensor = new five.Sensor("A0")
+  var liftRelay = new five.Relay(13);
+  var liftRelay = new five.Relay(12);
+
+  this.repl.inject({
+    lift: liftRelay
+  });
 
   sensor.within([100, 150], function(){
 
       console.log("Amplify Finger up");
       contract = !contract;
 
+      liftRelay.toggle();
 
       if(contract){
-        liftRelay.on();
         console.log("lift up");
       }else{
-        liftRelay.on();
         console.log("lift down");
       }
-
-
-      this.repl.inject({
-        relay: liftRelay
-      });
   });
+
+
 });
+ 
