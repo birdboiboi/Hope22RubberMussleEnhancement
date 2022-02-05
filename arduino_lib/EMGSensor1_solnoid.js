aed0d0d813290774e3bcd11f6910980a04ca4689
jordan <jai36@drexel.edu>
2022-02-05 18:52:53 -0500

    tweaked threshold and second threshold

--- arduino_lib/EMGSensor1_solnoid.js
+++ arduino_lib/EMGSensor1_solnoid.js
@@ -21,7 +21,7 @@ board.on("ready", function(){
   
 
   sensorForearm.on("change", function () {
-    console.log(this.scaleTo(0,50));
+    console.log(this.value);
     var val = this.value;
     if (val > 100){
       liftRelay.toggle();
