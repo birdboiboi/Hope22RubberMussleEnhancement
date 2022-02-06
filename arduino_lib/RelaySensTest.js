40d99e9208eb5cdfaf8c374eb48b76afc7c84aa1
Imsandysh <35186843+Imsandysh@users.noreply.github.com>
2022-02-06 01:55:03 -0500

    Update RelaySensTest.js

--- arduino_lib/RelaySensTest.js
+++ arduino_lib/RelaySensTest.js
@@ -1,13 +1,11 @@
 var five = require("johnny-five");
-var SerialPort = require("serialport").SerialPort;
-var five = require("johnny-five");
-var board = new five.Board({
-  port: "/dev/ttyACM0", 
-    baudrate: 900,
-    buffersize: 1
-  });
+var board = new five.Board();
+
 board.on("ready", function() {
-  var sensor = new five.Sensor("A0")
+  var sensor = new five.Sensor({
+    pin: "A0",
+    freq : 1000
+  });
   var relay = new five.Relay(13);
 
   sensor.on("change", function () {
@@ -15,6 +13,7 @@ board.on("ready", function() {
     var val = this.value;
     if (val > 200){
       relay.close()
+    
       }
 
     });
