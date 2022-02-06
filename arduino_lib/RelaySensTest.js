54a68ab2116d195477017c3f5b27e0d6f5561a45
Imsandysh <35186843+Imsandysh@users.noreply.github.com>
2022-02-05 23:19:46 -0500

    Create RelaySensTest.js

---
+++ arduino_lib/RelaySensTest.js
@@ -0,0 +1,22 @@
+var five = require("johnny-five");
+var board = new five.Board();
+
+board.on("ready", function() {
+  var sensor = new five.Sensor("A0")
+  var relay = new five.Relay(13);
+
+  sensor.on("change", function () {
+    console.log(this.value);
+    var val = this.value;
+
+    if (val > 100){
+      relay.open()
+      }else{
+        relay.close()
+      }
+    });
+      this.repl.inject({
+          relay: relay
+     
+    })
+  });
