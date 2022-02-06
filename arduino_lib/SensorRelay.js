73a41c83ba33c7d519977fdbeeae08777de3358e
Imsandysh <35186843+Imsandysh@users.noreply.github.com>
2022-02-05 20:01:24 -0500

    Update SensorRelay.js

--- arduino_lib/SensorRelay.js
+++ arduino_lib/SensorRelay.js
@@ -1,7 +1,7 @@
 var five = require("johnny-five");
 var board = new five.Board();
 
-board.on("ready", () => {
+board.on("ready", function() {
   var sensor = new five.Sensor("A0")
   var relay = new five.Relay(13);
 
