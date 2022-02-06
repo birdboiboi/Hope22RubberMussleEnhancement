99873569189bf816a05f23c9aa16991c4dda26ee
Imsandysh <35186843+Imsandysh@users.noreply.github.com>
2022-02-06 10:03:45 -0500

    Update SensorRelay.js

--- arduino_lib/SensorRelay.js
+++ arduino_lib/SensorRelay.js
@@ -2,40 +2,26 @@ var five = require("johnny-five");
 var board = new five.Board();
 
 board.on("ready", function() {
-  var sensor1 = new five.Sensor({
+  var sensor = new five.Sensor({
     pin: "A0",
     freq : 1000
   });
-  var sensor2 = new five.Sensor({
-    pin: "A7"
-  });
-  var relay1 = new five.Relay(13);
-  var relay2 = new five.Relay(12);
+  var relay = new five.Relay(13);
 
-  sensor1.on("change", function () {
+  sensor.on("change", function () {
     console.log(this.value);
-    var val1 = this.value;
-    if (val1 > 200){
-      relay1.close()
+    var val = this.value;
+    if (val > 200){
+      relay.close()
     }
      else{
-      relay1.open()
+      relay.open()
       
     
       }
-      });
-      sensor2.on("change", function () {
-        console.log(this.value);
-        var val2 = this.value;
-        if (val2 > 200){
-          relay2.close()
-        }
-         else{
-          relay2.open()
-          
-        
-          }
-   
-   
+
+    });
+      this.repl.inject({
+          relay: relay  
+    })
   });
-});
