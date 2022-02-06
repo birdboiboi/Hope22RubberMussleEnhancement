d84fcf22f2b1b4df210ce83683134cb3f33208d2
jordan <jai36@drexel.edu>
2022-02-05 20:26:45 -0500

    added time delay extras

--- arduino_lib/SensorRelay.js
+++ arduino_lib/SensorRelay.js
@@ -2,24 +2,37 @@ var five = require("johnny-five");
 var board = new five.Board();
 
 board.on("ready", function() {
-  var sensor = new five.Sensor("A0")
+  var sensor = new five.Sensor(
+                                {pin:"A0",
+                                  threshold:200    })
   var relay = new five.Relay(13);
-
+  var setDelay = false;
+  var open = false;
   sensor.on("change", function () {
     console.log(this.value);
     var val = this.value;
 
-    if (val > 400){
-      setInterval(() => {
-        relay.open();
-      }, 1500);
-      
+    if (val > 300){
+      open = true;
+      }else{
+        open = false;
+      }
+    });
+
+    if (setDelay){
+      if(open){
+        setInterval(() => {
+          relay.open();
+          console.log("open")
+        },1500);
       }else{
         setInterval(() => {
           relay.close();
-        }, 1500);
+          console.log("close")
+        },1500);
       }
-    });
+
+    }
 
       this.repl.inject({
           relay: relay
